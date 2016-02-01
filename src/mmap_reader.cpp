9a2f46ff6651a9e618668ebdd3d5bb5323202f1d
Bartosz Stebel <bartoszstebel@gmail.com>
2016-02-02 00:27:43 +0100

    WIP: basic read/write classes
    Change-Id: I4f5cacc9fa07d5c9496b008c83388a268c8db7c8

---
+++ src/mmap_reader.cpp
@@ -0,0 +1,87 @@
+#include "mmap_reader.h"
+#include <cstring>
+
+int mmap_reader::open(const char *path) {
+    if(stream_base::open(path) < 0)
+        return -1;
+    //TODO those three are copied from stream_reader, dedup?
+    struct stat st;
+    if(0 != fstat(fd, &st)) {
+        perror("fstat");
+        return -1;
+    }
+    if(0 != lseek(fd, 0, SEEK_SET)) {
+        perror("lseek");
+        return -1;
+    }
+    if(st.st_size < header_size) {
+        fprintf(stderr, "Input file too short.\n");
+        return -1;
+    }
+    //end todo
+    mmap_buffer_size = st.st_size;
+    //TODO might want to madvise(MADV_WILLNEED) somewhere
+    mmap_buffer = (uint8_t*) mmap(nullptr, mmap_buffer_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if(mmap_buffer == MAP_FAILED) {
+        perror("mmap");
+        return -1;
+    }
+    //load_file_header requires alignment, but this header itself is designed
+    //to be aligned properly, and mmap_buffer is aligned to page size.
+    if(load_file_header(mmap_buffer, st.st_size))
+        return keyframe_count * frames_per_kf;
+
+    return keyframe_count * frames_per_kf;
+}
+
+void *mmap_reader::load_header(){
+    header_proto_size = kf0_off - hdr_data_offset;
+    header_proto = mmap_buffer + hdr_data_offset;
+    return header_proto;
+}
+
+void *mmap_reader::get_keyframe_data(offset_t n, size_t *size) {
+    uint8_t* kf_ptr = mmap_buffer + offset_for_kf(n);
+    /// !!!!!! TODO THIS MAY BE *NOT* ALIGNED
+    *size = kfhdr_read_data_size(kf_ptr);
+    return kf_ptr + kf_header_size;
+}
+
+std::vector<std::pair<void*, size_t>> mmap_reader::get_deltaframes_for_kf(offset_t keyframe) {
+    std::vector<std::pair<void*, size_t>> ret;
+    uint8_t *ptr = mmap_buffer + kfhdr_read_deltas(mmap_buffer + offset_for_kf(keyframe));
+    for(unsigned i = 0; i < frames_per_kf; i++) {
+        delta_size_t cur_size = readbuf_unaligned<delta_size_t>(ptr);
+        ptr += sizeof(delta_size_t);
+        void *data = ptr;
+        ptr += cur_size;
+        assert(ptr <= mmap_buffer + mmap_buffer_size);
+        ret.push_back({data, cur_size});
+    }
+    return ret;
+}
+
+void mmap_reader::find_keyframe(offset_t n){
+    for(unsigned i = 0; i < skiplist_height; i++) {
+        offset_t x = n - (1<<i);
+        if(has_kf_offset(x)) {
+            fill_offsets_from_skiplist(mmap_buffer + offset_for_kf(x), x);
+            return;
+            //TODO czy tu na pewno nie ma buga, magic
+        }
+    }
+    offset_t curr = 0;
+    void* curr_ptr = mmap_buffer + kf0_off;
+    while(curr < n) {
+        fill_offsets_from_skiplist(curr_ptr, curr);
+        for(int i = skiplist_height - 1; i >= 0; i--) {
+            if(curr + (1 << i) <= n) {
+                curr += 1 << i;
+                curr_ptr = mmap_buffer + kfhdr_read_skiplist(curr_ptr, i);
+                assert(curr_ptr <= mmap_buffer + mmap_buffer_size);
+                break;
+            }
+        }
+    }
+    assert(curr == n);
+}
---
+++ src/stream_base.h
@@ -0,0 +1,135 @@
+#ifndef _STREAM_BASE_H
+#define _STREAM_BASE_H
+#include <type_traits>
+#include <unordered_map>
+#include <fcntl.h>
+#include <vector>
+
+#include "common.h"
+
+class stream_base {
+    public:
+    enum class open_mode { READ_ONLY, READ_WRITE };
+    private:
+    const open_mode mode;
+    //this is private to ensure nobody reads from it directly
+    //always use offset_for_kf
+    std::unordered_map<offset_t, offset_t> keyframe_offsets;
+
+    protected:
+    int fd;
+    uint32_t frames_per_kf;
+    offset_t file_size,
+             hdr_data_offset,
+             kf0_off,
+             keyframe_count;
+    //This pointer is *NOT* freed by stream_base.
+    //Child classes must handle this in their destructors.
+    void *header_proto;
+    size_t header_proto_size;
+
+    stream_base(open_mode mode):
+        mode(mode), fd(-1),
+        frames_per_kf(0),
+        file_size(0), kf0_off(0), keyframe_count(0),
+        header_proto(nullptr),
+        header_proto_size(0)
+    {
+    }
+    virtual ~stream_base() {
+        if(fd != -1)
+            close(fd);
+    }
+    static inline offset_t kfhdr_read_skiplist(const void *buf, uint8_t i){
+        //return readbuf_aligned<offset_t>((uint8_t*)buf + sizeof(offset_t) * (2 + i));
+        return readbuf_unaligned<offset_t>((uint8_t*)buf + sizeof(offset_t) * (2 + i));
+    }
+    static inline uint32_t kfhdr_read_data_size(const void *buf) {
+        //return readbuf_aligned<uint32_t>((uint8_t*)buf + sizeof(offset_t) * (skiplist_height + 2));
+        return readbuf_unaligned<uint32_t>((uint8_t*)buf + sizeof(offset_t) * (skiplist_height + 2));
+    }
+    static inline offset_t kfhdr_read_deltas(const void *buf) {
+        //return readbuf_aligned<offset_t>((uint8_t*)buf + sizeof(offset_t));
+        return readbuf_unaligned<offset_t>((uint8_t*)buf + sizeof(offset_t));
+    }
+    bool load_file_header(const uint8_t *hdr_buf, offset_t fsize) {
+        if(0 != memcmp(hdr_buf, magic, magic_size)) {
+            fprintf(stderr, "Invalid magic at beginning of file.\n");
+            return false;
+        }
+        //potential extra padding would be here
+        static_assert(sizeof(offset_t) <= 8, "TODO padding after magic");
+
+        file_size = readbuf_aligned<uint64_t>(hdr_buf + 8);
+        if(fsize != file_size) {
+            fprintf(stderr, "File size not consistent with data in header.\n");
+            return false;
+        }
+        hdr_data_offset = readbuf_aligned<uint64_t>(hdr_buf + 8*2);
+        kf0_off = readbuf_aligned<uint64_t>(hdr_buf + 8*3);
+        keyframe_count = readbuf_aligned<uint64_t>(hdr_buf + 8*4);
+        frames_per_kf = readbuf_aligned<uint32_t>(hdr_buf + 8*5);
+        if(hdr_data_offset > file_size - header_size ||
+                kf0_off > file_size - header_size ||
+                hdr_data_offset > kf0_off) {
+            fprintf(stderr, "Invalid offsets in header.\n");
+            return false;
+        }
+#ifdef DEBUG
+        fprintf(stderr, "_HDR: hdr_d_off %ld, kf0 %ld, kfc %ld, fpkf %d\n", hdr_data_offset, kf0_off, keyframe_count, frames_per_kf);
+#endif
+        keyframe_offsets[0] = kf0_off;
+        return true;
+    }
+    void fill_offsets_from_skiplist(const void *kfhdr, offset_t n) {
+        for(unsigned i = 0; i < skiplist_height; i++){
+            offset_t tmp = kfhdr_read_skiplist(kfhdr, i);
+            if(tmp != 0) {
+                offset_t num = n + (1 << i);
+                assert(num < keyframe_count);
+                keyframe_offsets.emplace(num, tmp);
+            }
+        }
+    }
+    inline void set_kf_offset(offset_t k, offset_t v) {
+#ifdef DEBUG
+        assert(keyframe_offsets.count(k) == 0);
+#endif
+        keyframe_offsets.emplace(k, v);
+    }
+    //required for implementations of find_keyframe
+    inline bool has_kf_offset(offset_t n) {
+        return keyframe_offsets.count(n) == 1;
+    }
+    public:
+    virtual int open(const char *path) {
+        if(mode == open_mode::READ_WRITE)
+            fd = ::open(path, O_RDWR | O_CREAT | O_EXCL, 0666);
+        else
+            fd = ::open(path, O_RDONLY);
+        if(fd)
+            return 0;
+        else
+            return -1;
+    }
+    virtual void find_keyframe(offset_t n) = 0;
+    inline offset_t offset_for_kf(offset_t n) {
+        if(!keyframe_offsets.count(n))
+            find_keyframe(n);
+        return keyframe_offsets[n];
+    }
+    void *get_header(size_t *s) {
+        if(!header_proto)
+            load_header();
+        *s = header_proto_size;
+        return header_proto;
+    }
+    // The returned pointer is managed by this class, do not free it
+    virtual void *load_header() = 0;
+    virtual void *get_keyframe_data(offset_t n, size_t *size) = 0;
+    virtual std::vector<std::pair<void*, size_t>> get_deltaframes_for_kf(offset_t keyframe) = 0;
+
+};
+
+
+#endif
