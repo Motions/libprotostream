71965c05c6575b192075283dcd26d05838365e71
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-02-28 19:47:42 +0100

    Code rewrite
    Change-Id: Id56c8568d95ac0fca4618b7f915d8b4a4c444cd1

---
+++ src/mmap_backend.cpp
@@ -0,0 +1,19 @@
+#include "mmap_backend.h"
+
+#include <sys/mman.h>
+
+namespace protostream {
+
+mmap_backend::mmap_backend(const char *path)
+        : file{path}, buffer_size{file.size()},
+          buffer{static_cast<uint8_t *>(mmap(nullptr, buffer_size, PROT_READ, MAP_PRIVATE, file.fd, 0))} {
+    if (buffer == MAP_FAILED) {
+        throw std::system_error(errno, std::system_category(), "mmap");
+    }
+}
+
+mmap_backend::~mmap_backend() {
+    munmap(buffer, buffer_size);
+}
+
+}
