f38524251e9460913f8bf493b2d182d4b0945930
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-02-28 19:47:42 +0100

    Code rewrite
    Change-Id: Id56c8568d95ac0fca4618b7f915d8b4a4c444cd1

---
+++ src/header.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "common.h"
+#include "header/generic_header.h"
+#include "header/magic_value.h"
+#include "header/placeholder.h"
+#include "header/with_offset.h"
+
+namespace protostream {
+
+namespace fields {
+inline namespace file_header {
+
+static constexpr const char magic_value[] = "PROTOSTR";
+static constexpr std::size_t magic_size = sizeof(magic_value) - 1;
+static_assert(magic_size == 8, "Invalid magic size");
+
+struct magic_field : public detail::magic_value<magic_value, magic_size, 0> { };
+struct file_size : public detail::with_offset<offset_t, 8> { };
+struct proto_header_offset : public detail::with_offset<offset_t, 8 * 2> { };
+struct kf0_offset : public detail::with_offset<offset_t, 8 * 3> { };
+struct keyframe_count : public detail::with_offset<offset_t, 8 * 4> { };
+struct frame_count : public detail::with_offset<offset_t, 8 * 5> { };
+struct frames_per_kf : public detail::with_offset<uint32_t, 8 * 6> { };
+
+}
+}
+
+struct file_header : public detail::generic_header<file_header,
+        fields::file_header::magic_field,
+        fields::file_header::file_size,
+        fields::file_header::proto_header_offset,
+        fields::file_header::kf0_offset,
+        fields::file_header::keyframe_count,
+        fields::file_header::frame_count,
+        fields::file_header::frames_per_kf
+> { };
+
+namespace fields {
+inline namespace reduced_keyframe_header {
+
+static constexpr unsigned skiplist_height = 10;
+
+static constexpr offset_t skiplist_offset(unsigned level = 0) {
+    return 8 * 2 + 8 * level;
+}
+
+struct kf_num : public detail::with_offset<offset_t, 0> { };
+struct delta_offset : public detail::with_offset<offset_t, 8> { };
+struct skiplist_placeholder : public detail::placeholder<8 * skiplist_height, 8 * 2> { };
+struct kf_size : public detail::with_offset<uint32_t, 8 * 2 + 8 * skiplist_height> { };
+
+}
+}
+
+struct reduced_keyframe_header : public detail::generic_header<reduced_keyframe_header,
+        fields::reduced_keyframe_header::kf_num,
+        fields::reduced_keyframe_header::delta_offset,
+        fields::reduced_keyframe_header::skiplist_placeholder,
+        fields::reduced_keyframe_header::kf_size
+> { };
+
+}
---
+++ src/utils.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include <endian.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdint>
+#include <cassert>
+#include <cstring>
+
+#include <type_traits>
+
+namespace protostream {
+
+namespace detail {
+
+/** Logical AND of conditions
+ *
+ * To be replaced with std::conjunction when it is finally available
+ */
+template<class...>
+struct conjunction;
+
+template<>
+struct conjunction<> : std::true_type {
+};
+
+template<class Head, class... Tail>
+struct conjunction<Head, Tail...> : std::conditional_t<Head::value, conjunction<Tail...>, Head> {
+};
+
+template<typename T>
+T betoh(const T);
+
+template<>
+inline std::uint64_t betoh<std::uint64_t>(const std::uint64_t x) {
+    return be64toh(x);
+}
+
+template<>
+inline std::uint32_t betoh<std::uint32_t>(const std::uint32_t x) {
+    return be32toh(x);
+}
+
+template<>
+inline std::uint16_t betoh<std::uint16_t>(const std::uint16_t x) {
+    return be16toh(x);
+}
+
+template<typename T>
+T htobe(const T);
+
+template<>
+inline std::uint64_t htobe<std::uint64_t>(const std::uint64_t x) {
+    return htobe64(x);
+}
+
+template<>
+inline std::uint32_t htobe<std::uint32_t>(const std::uint32_t x) {
+    return htobe32(x);
+}
+
+template<>
+inline std::uint16_t htobe<std::uint16_t>(const std::uint16_t x) {
+    return htobe16(x);
+}
+
+template<typename T>
+inline T readbuf_aligned(const void* ptr) {
+    assert((uintptr_t) ptr % sizeof(T) == 0);
+    return betoh<T>(*(T*) ptr);
+
+}
+
+template<typename T>
+inline void writebuf(void* ptr, const T x) {
+    assert((uintptr_t) ptr % sizeof(T) == 0);
+    *(T*) ptr = htobe<T>(x);
+}
+
+template<typename T>
+T readbuf_unaligned(const void* a) {
+    T val;
+    memcpy(&val, a, sizeof(T));
+    val = betoh(val);
+    return val;
+}
+
+template<class Ptr>
+auto as_ptr(Ptr& ptr) {
+    return ptr.get();
+}
+
+template<class T>
+auto as_ptr(T* ptr) {
+    return ptr;
+}
+
+}
+
+}
