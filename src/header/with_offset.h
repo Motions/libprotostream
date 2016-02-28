3a881215b95b9d09f6506150dc22e38156a82915
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-02-28 19:47:42 +0100

    Code rewrite
    Change-Id: Id56c8568d95ac0fca4618b7f915d8b4a4c444cd1

---
+++ src/header/with_offset.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "common.h"
+#include "utils.h"
+
+namespace protostream {
+namespace detail {
+
+/** A wrapper around the type `T`, enriching it with offset information. */
+template<class T, offset_t Offset>
+struct with_offset {
+    using type = T;
+    static constexpr offset_t offset = Offset;
+    static constexpr size_t size = sizeof(type);
+
+    type value;
+
+    constexpr with_offset(type value = 0) : value{value} { }
+
+    constexpr operator type() const {
+        return value;
+    }
+
+    static with_offset read(const std::uint8_t* buffer) {
+        return readbuf_aligned<type>(buffer + offset);
+    }
+
+    template<class Backend>
+    static with_offset read(const Backend& backend, offset_t file_offset) {
+        return backend.template read_num<type>(file_offset + offset);
+    }
+
+    template<class... Args>
+    void read_self(Args&& ... args) {
+        value = with_offset::read(std::forward<Args>(args)...);
+    }
+
+    void write(uint8_t* buffer) const {
+        writebuf(buffer + offset, value);
+    }
+
+    template<class Backend>
+    void write(Backend& backend, offset_t file_offset) const {
+        backend.write_num(file_offset + offset, value);
+    }
+};
+
+}
+}
