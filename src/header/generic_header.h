6e738b57b06aa101734aaacf1463c2f6d670f418
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-12 13:21:26 +0100

    Code rewrite
    Change-Id: Id56c8568d95ac0fca4618b7f915d8b4a4c444cd1

---
+++ src/header/generic_header.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <cstddef>
+
+#include <algorithm>
+#include <tuple>
+
+namespace protostream{
+namespace detail {
+
+namespace overlap_check {
+
+template<class Field1, class Field2>
+struct check_pair {
+    static_assert(!(Field1::offset <= Field2::offset &&
+                    Field1::offset + Field1::size > Field2::offset), "fields overlap");
+};
+
+template<class Field>
+struct check_pair<Field, Field> { };
+
+template<class Field, class... Fields>
+struct check_one_to_many : check_pair<Field, Fields> ... { };
+
+template<class... Fields>
+struct check_all_pairs : check_one_to_many<Fields, Fields...> ... {
+};
+
+}
+
+template<class Derived, class... Fields>
+class generic_header {
+    static constexpr overlap_check::check_all_pairs<Fields...> overlap_checker{};
+
+public:
+    static constexpr std::size_t size = std::max({(Fields::offset + Fields::size)...});
+
+    template<class... Args>
+    void read_self(Args&&... args) {
+        /* Call read_self for every field */
+        int _[] = {(std::get<Fields>(fields).read_self(args...), 0)...};
+        (void) _;
+    }
+
+    template<class... Args>
+    static Derived read(Args&& ... args) {
+        Derived result;
+        result.read_self(std::forward<Args>(args)...);
+        return result;
+    }
+
+    template<class... Args>
+    void write(Args&&... args) const {
+        /* Call write for every field */
+        int _[] = {(std::get<Fields>(fields).write(args...), 0)...};
+        (void) _;
+    }
+
+    template<class Field>
+    const auto& get() const {
+        return std::get<Field>(fields).value;
+    }
+
+    template<class Field>
+    auto& get() {
+        return std::get<Field>(fields).value;
+    }
+
+private:
+    std::tuple<Fields...> fields;
+};
+
+}
+}
