5042ff2b8258bb33c4bcbbd80291f171dded23af
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-17 21:28:46 +0100

    Move file operations to posix_file_handler
    Change-Id: I0b611cad41598e7b79ba83b58390ef9fb8708520

--- include/mmap_backend.h
+++ include/mmap_backend.h
@@ -1,11 +1,8 @@
 #pragma once
 
-#include "config.h"
 #include "file_backend.h"
 #include "posix_file_handler.h"
 
-#include <fcntl.h>
-#include <sys/mman.h>
 
 namespace protostream {
 
@@ -18,20 +15,12 @@ public:
     /** Raw pointers are returned directly -- no deallocation is needed,
      *  because the whole file is memory-mapped
      */
-    using pointer_type = std::uint8_t*;
+    using pointer_type = const std::uint8_t*;
 
     mmap_backend(const char* path)
-            : file{path}, used_size{file.size()}, file_size{used_size} {
-        do_mmap();
-    }
+            : file{path}, used_size{file.size()}, file_size{used_size}, buffer{file.mmap(file_size)} { }
 
-    ~mmap_backend() {
-        munmap(buffer, file_size);
-
-        if (used_size != file_size) {
-            ftruncate(file.fd, used_size);
-        }
-    }
+    ~mmap_backend();
 
     mmap_backend(const mmap_backend&) = delete;
 
@@ -44,7 +33,7 @@ public:
     template<class T>
     void read_small(offset_t offset, T* into) const {
         // TODO: should we care about data alignment?
-        *into = *reinterpret_cast<T*>(read(offset, sizeof(T)));
+        *into = *reinterpret_cast<const T*>(read(offset, sizeof(T)));
     }
 
     pointer_type read(offset_t offset, size_t length) const {
@@ -71,86 +60,24 @@ public:
 private:
     posix_file_handler<mode> file;
     std::size_t used_size, file_size;
-    std::uint8_t* buffer;
+    typename posix_file_handler<mode>::buffer_type buffer;
 
     void check_expand(std::size_t new_end);
-
-    void do_mmap() {
-        if (file_size == 0) {
-            buffer = nullptr;
-            return;
-        }
-
-        const auto mmap_prot = mode == file_mode_t::READ_ONLY
-                               ? PROT_READ : (PROT_WRITE | PROT_READ);
-        const auto mmap_flags = mode == file_mode_t::READ_ONLY
-                                ? MAP_PRIVATE : MAP_SHARED;
-
-        auto buf = mmap(nullptr, file_size, mmap_prot, mmap_flags, file.fd, 0);
-        if (buf == MAP_FAILED) {
-            throw std::system_error{errno, std::system_category(), "mmap"};
-        }
-        buffer = static_cast<std::uint8_t*>(buf);
-    }
-
-    void do_munmap() {
-        if (!buffer) {
-            return;
-        }
-
-        if (munmap(buffer, file_size)) {
-            throw std::system_error{errno, std::system_category(), "munmap"};
-        }
-        buffer = nullptr;
-    }
-
-    /** Expands the mapping to `new_size`, sets `file_size` to `new_size` */
-    void do_mremap(std::size_t new_size) {
-#ifdef HAVE_MREMAP
-        if (file_size == 0) {
-            file_size = new_size;
-            do_mmap();
-        }
-        else {
-            auto buf = mremap(buffer, file_size, new_size, MREMAP_MAYMOVE);
-            if (buf == MAP_FAILED) {
-                throw std::system_error{errno, std::system_category(), "mremap"};
-            }
-            buffer = static_cast<std::uint8_t*>(buf);
-            file_size = new_size;
-        }
-#else
-        do_munmap();
-        file_size = new_size;
-        do_mmap();
-#endif
-    }
-
-    /** Expands the backing file by `expand_by` */
-    void do_fallocate(std::size_t expand_by) {
-#if defined(HAVE_FALLOCATE)
-        if (fallocate(file.fd, 0, file_size, expand_by) != 0) {
-            throw std::system_error{errno, std::system_category(), "fallocate"};
-        }
-#elif defined(HAVE_POSIX_FALLOCATE)
-        if (auto ret = posix_fallocate(file.fd, file_size, expand_by)) {
-            throw std::system_error{ret, std::system_category(), "posix_fallocate"};
-        }
-#elif defined(HAVE_F_PREALLOCATE)
-        fstore_t store{F_ALLOCATECONTIG, F_PEOFPOSMODE, 0, static_cast<off_t>(expand_by), 0};
-        if (fcntl(file.fd, F_PREALLOCATE, &store) == -1) {
-            throw std::system_error{errno, std::system_category(), "fcntl"};
-        }
-#else
-        char buf[] = {0};
-        switch (auto ret = pwrite(file.fd, buf, 1, file_size + expand_by)) {
-            case 1: return;
-            case -1: throw std::system_error{errno, std::system_category(), "pwrite"};
-            default: throw std::runtime_error{"Write returned " + std::to_string(ret)};
-        }
-#endif
-    }
 };
+
+template<>
+mmap_backend<file_mode_t::READ_ONLY>::~mmap_backend() {
+    file.munmap(buffer, file_size);
+}
+
+template<>
+mmap_backend<file_mode_t::READ_APPEND>::~mmap_backend() {
+    file.munmap(buffer, file_size);
+
+    if (used_size != file_size) {
+        file.truncate(used_size);
+    }
+}
 
 template<>
 inline void mmap_backend<file_mode_t::READ_APPEND>::check_expand(std::size_t new_end) {
@@ -166,8 +93,9 @@ inline void mmap_backend<file_mode_t::READ_APPEND>::check_expand(std::size_t new
     expand_by /= expansion_granularity;
     expand_by *= expansion_granularity;
 
-    do_fallocate(expand_by);
-    do_mremap(file_size + expand_by);
+    file.expand(file_size, expand_by);
+    buffer = file.mremap(buffer, file_size, file_size + expand_by);
+    file_size += expand_by;
     used_size = new_end;
 }
 
