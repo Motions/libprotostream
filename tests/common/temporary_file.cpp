9856aaeea62088844060e78997fdb5c8c5d72743
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-18 00:17:55 +0100

    Basic tests
    Change-Id: I33b38f4e1527bf6ec934f1bed5eade865593e5bf

---
+++ tests/common/temporary_file.cpp
@@ -0,0 +1,49 @@
+#include "temporary_file.h"
+#include "file_operations.h"
+
+#include <iostream>
+
+temporary_file::temporary_file() : path{boost::filesystem::unique_path()}, released{false} { }
+
+temporary_file::temporary_file(const std::string& initial_contents) : temporary_file{} {
+    auto stream = std::ofstream{filepath(), std::ofstream::binary};
+    stream << initial_contents;
+}
+
+const char* temporary_file::filepath() const {
+    return path.native().c_str();
+}
+
+temporary_file::temporary_file(temporary_file&& that) : path{std::move(that.path)}, released{false} { }
+
+temporary_file& temporary_file::operator=(temporary_file&& that) {
+    path = std::move(that.path);
+    released = false;
+    that.released = true;
+    return *this;
+}
+
+temporary_file::~temporary_file() {
+    if (!released) {
+        close();
+    }
+}
+
+void temporary_file::close() {
+    if (released) {
+        throw std::logic_error{"Closing a released temporary file"};
+    }
+
+    released = true;
+    if (boost::filesystem::exists(path)) {
+        boost::filesystem::remove(path);
+    }
+}
+
+std::string temporary_file::contents() const {
+    return file_contents(path);
+}
+
+std::size_t temporary_file::size() const {
+    return boost::filesystem::file_size(path);
+}
