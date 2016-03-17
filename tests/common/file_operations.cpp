705f90d40eb710ef4ce179a0a68ef0a5d49a8362
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-18 00:17:55 +0100

    Basic tests
    Change-Id: I33b38f4e1527bf6ec934f1bed5eade865593e5bf

---
+++ tests/common/file_operations.cpp
@@ -0,0 +1,6 @@
+#include "file_operations.h"
+
+std::string file_contents(const boost::filesystem::path &path) {
+    std::ifstream stream{path.native().c_str(), std::ifstream::binary};
+    return {std::istreambuf_iterator<char>{stream}, std::istreambuf_iterator<char>{}};
+}
---
+++ tests/integrationtests/common.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "stream.h"
+#include "mmap_backend.h"
+#include "posix_file_backend.h"
+#include "cache.h"
+
+#include <gtest/gtest.h>
+
+namespace types {
+using namespace protostream;
+
+struct string_factory {
+    using type = std::string;
+
+    static type build(const char* start, std::size_t len) {
+        return std::string(start, start + len);
+    }
+
+    template<class Ptr>
+    static type build(Ptr&& ptr, std::size_t len) {
+        return build(reinterpret_cast<const char*>(detail::as_ptr(ptr)), len);
+    }
+};
+
+using mmap_writer = stream<
+        with_backend<mmap_backend<file_mode_t::READ_APPEND>>,
+        with_cache<offsets_only_cache>,
+        with_keyframe_factory<string_factory>,
+        with_delta_factory<string_factory>,
+        with_proto_header_factory<string_factory>>;
+
+using stream_writer = stream<
+        with_backend<posix_file_backend<file_mode_t::READ_APPEND>>,
+        with_cache<full_cache>,
+        with_keyframe_factory<string_factory>,
+        with_delta_factory<string_factory>,
+        with_proto_header_factory<string_factory>>;
+
+using mmap_reader = stream<
+        with_backend<mmap_backend<file_mode_t::READ_ONLY>>,
+        with_cache<offsets_only_cache>,
+        with_keyframe_factory<string_factory>,
+        with_delta_factory<string_factory>,
+        with_proto_header_factory<string_factory>>;
+
+using stream_reader = stream<
+        with_backend<posix_file_backend<file_mode_t::READ_ONLY>>,
+        with_cache<full_cache>,
+        with_keyframe_factory<string_factory>,
+        with_delta_factory<string_factory>,
+        with_proto_header_factory<string_factory>>;
+
+
+using read_streams = testing::Types<
+        types::mmap_reader, types::mmap_writer, types::stream_reader, types::stream_writer>;
+
+using write_streams = testing::Types<
+        types::mmap_writer, types::stream_writer>;
+}
