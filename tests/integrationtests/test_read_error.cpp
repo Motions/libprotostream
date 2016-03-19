9ed178be2d105423c2d659e5a3d8a4544b086b8e
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-19 17:48:00 +0100

    More tests, exception fixes
     * Added more tests
     * Fixed the exception thrown when opening an existing file smaller than
       the file header.
    
    Change-Id: I2803f0ac18beb3542b5bdac7e4aba6c6fad0c3d6

---
+++ tests/integrationtests/test_read_error.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include "type_list.h"
+#include "streams.h"
+#include "../common/temporary_file.h"
+
+template<class Stream>
+struct integration_read_error : public testing::Test { };
+
+TYPED_TEST_CASE(integration_read_error, type_list::unapply<streams::read_streams>::type<testing::Types>);
+
+TYPED_TEST(integration_read_error, empty_file) {
+    auto file = temporary_file{};
+    EXPECT_THROW(TypeParam{file.filepath()}, std::runtime_error);
+}
+
+TYPED_TEST(integration_read_error, wrong_file_size) {
+    EXPECT_THROW(TypeParam{"data/error/wrong_file_size.data"}, std::runtime_error);
+}
+
+TYPED_TEST(integration_read_error, wrong_proto_header_offset) {
+    EXPECT_THROW(TypeParam{"data/error/wrong_proto_header_offset.data"}, std::runtime_error);
+}
+
+TYPED_TEST(integration_read_error, wrong_kf0_offset) {
+    EXPECT_THROW(TypeParam{"data/error/wrong_kf0_offset.data"}, std::runtime_error);
+}
+
+TYPED_TEST(integration_read_error, proto_header_after_kf0) {
+    EXPECT_THROW(TypeParam{"data/error/proto_header_after_kf0.data"}, std::runtime_error);
+}
