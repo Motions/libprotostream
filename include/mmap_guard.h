963a0bbbae9198da6b6a3664f506a23273af474c
Krzysztof Pszeniczny <krzysztof.pszeniczny@gmail.com>
2016-03-23 20:13:47 +0100

    Add mmap_guard
    Change-Id: I6aaa5d34a0179f289c89a1949a6aee20a2b79522

---
+++ include/mmap_guard.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+
+namespace protostream {
+
+template<class Handler>
+class mmap_guard {
+public:
+    using buffer_type = typename Handler::buffer_type;
+
+    mmap_guard(Handler &handler) : mmap_guard{handler, handler.size()} { }
+
+    mmap_guard(Handler &handler, std::size_t size)
+            : handler{handler}, bufsize{size}, buffer{handler.mmap(size)} { }
+
+    ~mmap_guard() {
+        handler.munmap(buffer, bufsize);
+    }
+
+    void mremap(std::size_t new_size) {
+        buffer = handler.mremap(buffer, bufsize, new_size);
+        bufsize = new_size;
+    }
+
+    std::size_t size() const {
+        return bufsize;
+    }
+
+    buffer_type get() {
+        return buffer;
+    }
+
+    const buffer_type get() const {
+        return buffer;
+    }
+private:
+    Handler &handler;
+    std::size_t bufsize;
+    buffer_type buffer;
+};
+
+}
